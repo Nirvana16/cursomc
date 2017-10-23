9e5b82fc559c12526607ae0786631cd8fd350e97
Nirvana16 <zedanilo161@gmail.com>
2017-10-23 15:32:59 -0200

    Testando classe Categoria

--- src/main/java/com/jd/cursomc/resources/CatergoriaResource.java
+++ src/main/java/com/jd/cursomc/resources/CatergoriaResource.java
@@ -1,15 +1,28 @@
 package com.jd.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jd.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CatergoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest funcionando!";
+	public List <Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List <Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+				
+		return lista;
 	}
 }
